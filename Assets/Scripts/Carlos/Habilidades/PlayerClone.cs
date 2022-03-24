0eb83d11c8e089a7d90812a0f60fb4cb4486ae40
lmYourGod <carlos2003cjj@gmail.com>
2022-03-24 02:52:03 +0100

    Update Habilidades 2
    - He creado la habilidad de clonar
    - Estoy creando la habilidad de transformar enemigos

--- Assets/Scripts/Carlos/Habilidades/PlayerClone.cs
+++ Assets/Scripts/Carlos/Habilidades/PlayerClone.cs
@@ -1,18 +1,58 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerClone : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Variables
+    [SerializeField] private GameObject playerClone;
+    [SerializeField] private GameObject spawnerClone;
+    
+    private IEnumerator destruirClone;
+
+    private void Start()
     {
-        
+        // Añadimos a la variable la funcion que queremos que haga
+        destruirClone = TimeToDestroyClone();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        SpawnearClone();
         
+        // Si existe un objeto con el tag "PlayerClone" se moverá hacia delante
+        if (GameObject.FindWithTag("PlayerClone") != null)
+        {
+            GameObject.FindWithTag("PlayerClone").transform.Translate(Vector3.forward * 3 * Time.deltaTime);
+        }
+    }
+
+    private void SpawnearClone()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            // Paramos la corrutina DestruirClone
+            StopCoroutine(destruirClone);
+            
+            // Si existe un objeto con el nombre "PlayerClone" lo destruimos (esto nos sirve para solo tener un clon spawneado)
+            if (GameObject.FindWithTag("PlayerClone") != null)
+            {
+                // Destruimos el objeto con el tag "PlayerClone"
+                Destroy(GameObject.FindWithTag("PlayerClone"));
+            }
+            
+            // Spawneamos el prefab en la posicion del player y donde estemos mirando
+            Instantiate(playerClone, spawnerClone.transform.position, transform.rotation);
+            // Empezamos la corrutina DestruirClone
+            StartCoroutine(destruirClone);
+        }
+    }
+
+    private IEnumerator TimeToDestroyClone()
+    {
+        // Después de 6 segundos destruimos el objeto con el tag "PlayerClone"
+        yield return new WaitForSeconds(6);
+        Destroy(GameObject.FindWithTag("PlayerClone"));
     }
 }
---
+++ Assets/Scripts/Carlos/Habilidades/Polimorfo.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Polimorfo : MonoBehaviour
+{
+    // Variables
+    [SerializeField] private float distanceToClosestEnemy;
+    [SerializeField] private float distanceToEnemy;
+    [SerializeField] private Enemy ClosestEnemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FindClosestEnemy();
+    }
+
+    private void FindClosestEnemy()
+    {
+        distanceToClosestEnemy = Mathf.Infinity;
+        ClosestEnemy = null;
+        Enemy[] allEnemies = FindObjectsOfType<Enemy>();
+
+        foreach (Enemy currentEnemy in allEnemies)
+        {
+            distanceToEnemy = (currentEnemy.transform.position - transform.position).sqrMagnitude;
+            if (distanceToEnemy < distanceToClosestEnemy)
+            {
+                distanceToClosestEnemy = distanceToEnemy;
+                ClosestEnemy = currentEnemy;
+            }
+        }
+        
+        Debug.DrawLine(transform.position, ClosestEnemy.transform.position);
+    }
+}
