c0faf8a0816cfa75a77a991d937af6e41f6293c6
ImYourG0D <carlos2003cjj@gmail.com>
2022-03-29 19:20:11 +0200

    Update Habilidades 3
    - He empezado la habilidad de humo
    - He añadido los assets necesarios para el URP

---
+++ Assets/Scripts/Carlos/Habilidades/Clone/PlayerClone.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerClone : MonoBehaviour
+{
+    // Variables
+    [SerializeField] private GameObject playerClone;
+    [SerializeField] private GameObject spawnerClone;
+    
+    private IEnumerator destruirClone;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        SpawnearClone();
+        
+        // Añadimos a la variable la funcion que queremos que haga
+        destruirClone = TimeToDestroyClone();
+    }
+
+    private void SpawnearClone()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && GameObject.FindWithTag("PlayerClone") == null)
+        {
+            // Spawneamos el prefab en la posicion del player y donde estemos mirando
+            Instantiate(playerClone, spawnerClone.transform.position, transform.rotation);
+            
+            // Empezamos la corrutina DestruirClone
+            StartCoroutine(destruirClone);
+            
+            // Si existe un objeto con el nombre "PlayerClone" lo destruimos (esto nos sirve para solo tener un clon spawneado)
+            /*if (GameObject.FindWithTag("PlayerClone") != null)
+            {
+                // Destruimos el objeto con el tag "PlayerClone"
+                Destroy(GameObject.FindWithTag("PlayerClone"));
+                // Paramos la corrutina DestruirClone
+                StopCoroutine(destruirClone);
+            }*/
+        }
+        
+        // Si existe un objeto con el tag "PlayerClone" se moverá hacia delante
+        if (GameObject.FindWithTag("PlayerClone") != null)
+        {
+            GameObject.FindWithTag("PlayerClone").transform.Translate(Vector3.forward * 3 * Time.deltaTime);
+        }
+    }
+
+    private IEnumerator TimeToDestroyClone()
+    {
+        // Después de 6 segundos destruimos el objeto con el tag "PlayerClone"
+        yield return new WaitForSeconds(6);
+        Destroy(GameObject.FindWithTag("PlayerClone"));
+    }
+}
