23c16a8171f24c8e89406e3b77cbb1d119992922
OscarMontesinos <79588290+OscarMontesinos@users.noreply.github.com>
2022-03-29 16:44:04 +0200

    Arreglo final

--- Assets/Scripts/Oscar/Cazador.cs
+++ Assets/Scripts/Oscar/Cazador.cs
@@ -9,27 +9,24 @@ public class Cazador : MonoBehaviour
     public int state = 0;
     public float timePassed = 0;
     public float secondsToDismiss;
-    bool search;
 
     private void Update()
     {
-        if(target != null && search)
+        if(target != null)
         {
             state = 2;
             timePassed = 0;
-            search = true;
         }
         else
         {
             if (timePassed < secondsToDismiss)
             {
                 state = 1;
-                timePassed *= Time.deltaTime;
+                timePassed += Time.deltaTime;
             }
             else
             {
                 state = 0;
-                search = false;
             }
         }
     }
