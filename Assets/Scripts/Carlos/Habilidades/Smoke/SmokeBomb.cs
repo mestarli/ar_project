9c27e290f67a99f0bbd7125f222d721c1ed8965a
ImYourG0D <carlos2003cjj@gmail.com>
2022-04-05 17:18:28 +0200

    Update Habilidades 3
    - He acabado la habilidad de humo

--- Assets/Scripts/Carlos/Habilidades/Smoke/SmokeBomb.cs
+++ Assets/Scripts/Carlos/Habilidades/Smoke/SmokeBomb.cs
@@ -6,13 +6,17 @@ using UnityEngine.VFX;
 public class SmokeBomb : MonoBehaviour
 {
     // Variables
+    [Header("FUNCIONES BOMBA HUMO")]
     [SerializeField] private GameObject bombSmoke;
     [SerializeField] private GameObject spawnBombSmoke;
-    
+    [SerializeField] private bool canSpawnSmoke;
+    [SerializeField] private List<GameObject> smokesEffects;
+    [SerializeField] private VisualEffect[] smokeEffectsChildren;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        canSpawnSmoke = true;
     }
 
     // Update is called once per frame
@@ -25,23 +29,37 @@ public class SmokeBomb : MonoBehaviour
 
     private void ThrowSmokeBomb()
     {
-        if (Input.GetKeyDown(KeyCode.R))
+        // Si presionamos la tecla R y podemos spawnear el smoke iniciaremos la corrutina
+        if (Input.GetKeyDown(KeyCode.R) && canSpawnSmoke)
         {
+            canSpawnSmoke = false;
             StartCoroutine(waitToThrowBombSmoke());
         }
     }
 
     private IEnumerator waitToThrowBombSmoke()
     {
+        // Borramos lo que haya en la lista de smokes y esperamos 0.7s para lanzar la bomba de humo, añadimos ese prefab spawneado a la lista de smokes e
+        // iniciamos la corrutina para desvanecer el humo
+        smokesEffects.Clear();
         yield return new WaitForSeconds(0.7f);
-        Instantiate(bombSmoke, spawnBombSmoke.transform.position, spawnBombSmoke.transform.rotation);
+        var ParticleEffect = Instantiate(bombSmoke, spawnBombSmoke.transform.position, spawnBombSmoke.transform.rotation);
+        smokesEffects.Add(ParticleEffect);
         StartCoroutine(waitToDesapearSmokeBomb());
     }
     
     private IEnumerator waitToDesapearSmokeBomb()
     {
+        // Añadimos los visualEffects de humo que tienen los hijos del prefab de la lista smokesEffects a la lista smokeEffectsChildren y esperamos
+        // 10s para borrar la lista de smokes, después podremos volver a spawnear otra bomba de humo y se desvanecerá la anterior bomba lanzada
+        smokeEffectsChildren = smokesEffects[0].GetComponentsInChildren<VisualEffect>();
         yield return new WaitForSeconds(10);
-        bombSmoke.GetComponentsInChildren<VisualEffect>()[bombSmoke.GetComponentsInChildren<VisualEffect>().Length].pause = true;
+        smokesEffects.Clear();
+        canSpawnSmoke = true;   
+        foreach (var visualEffect in smokeEffectsChildren)
+        {
+            visualEffect.Stop();
+        }
     }
     
     #endregion
