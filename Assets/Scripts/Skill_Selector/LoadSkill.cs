78347541a7bc6add3e1280a1093f9fc9de559e1a
mestarli <97691723+mestarli@users.noreply.github.com>
2022-05-17 10:08:59 +0200

    Se cargan los scripts correspondientes

--- Assets/Scripts/Skill_Selector/LoadSkill.cs
+++ Assets/Scripts/Skill_Selector/LoadSkill.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -17,13 +18,27 @@ public class LoadSkill : MonoBehaviour
     public Image SelectedSkillImage3;
 
     public List<Sprite> Sprites;
+    private MonoBehaviour EnableSkill_01;
+    private MonoBehaviour EnableSkill_02;
+    private MonoBehaviour EnableSkill_03;
+    
+    [SerializeField]private List<string> Skills;
     
     void Start()
     {
        Skill_1 = PlayerPrefs.GetInt("SelectedSkill_1", SelectedSkill_1);
        Skill_2 = PlayerPrefs.GetInt("SelectedSkill_2", SelectedSkill_2);
        Skill_3 = PlayerPrefs.GetInt("SelectedSkill_3", SelectedSkill_3);
+       Skills.Add("PlayerClone");
+       Skills.Add("IceWall");
+       Skills.Add("Dash");
+       Skills.Add("Regeneracion");
+       Skills.Add("Invisibility");
+       Skills.Add("Blind");
+       Skills.Add("Polimorfo");
+       Skills.Add("SmokeBomb");
        PrinSelectedSkill();
+       LoadSkills();
     }
     
     public void PrinSelectedSkill()
@@ -31,5 +46,14 @@ public class LoadSkill : MonoBehaviour
         SelectedSkillImage1.sprite = Sprites[Skill_1];
         SelectedSkillImage2.sprite = Sprites[Skill_2];
         SelectedSkillImage3.sprite = Sprites[Skill_3];
+    }
+    
+    private void LoadSkills(){
+        EnableSkill_01= (MonoBehaviour) Player.Instance.GetComponent(Type.GetType(Skills[Skill_1]));
+        EnableSkill_02= (MonoBehaviour) Player.Instance.GetComponent(Type.GetType(Skills[Skill_2]));
+        EnableSkill_03= (MonoBehaviour) Player.Instance.GetComponent(Type.GetType(Skills[Skill_3]));
+        EnableSkill_01.enabled = true;
+        EnableSkill_02.enabled = true;
+        EnableSkill_03.enabled = true;
     }
 }
