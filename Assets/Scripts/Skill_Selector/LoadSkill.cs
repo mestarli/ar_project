d21894d8c78d4d007e6072da909a013502112af1
mestarli <97691723+mestarli@users.noreply.github.com>
2022-05-17 09:06:23 +0200

    UI > Mostrar habilidades seleccionadas

--- Assets/Scripts/Skill_Selector/LoadSkill.cs
+++ Assets/Scripts/Skill_Selector/LoadSkill.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LoadSkill : MonoBehaviour
 {
@@ -11,10 +12,24 @@ public class LoadSkill : MonoBehaviour
     public int Skill_2;
     public int Skill_3;
     
+    public Image SelectedSkillImage1;
+    public Image SelectedSkillImage2;
+    public Image SelectedSkillImage3;
+
+    public List<Sprite> Sprites;
+    
     void Start()
     {
        Skill_1 = PlayerPrefs.GetInt("SelectedSkill_1", SelectedSkill_1);
        Skill_2 = PlayerPrefs.GetInt("SelectedSkill_2", SelectedSkill_2);
        Skill_3 = PlayerPrefs.GetInt("SelectedSkill_3", SelectedSkill_3);
+       PrinSelectedSkill();
+    }
+    
+    public void PrinSelectedSkill()
+    {
+        SelectedSkillImage1.sprite = Sprites[Skill_1];
+        SelectedSkillImage2.sprite = Sprites[Skill_2];
+        SelectedSkillImage3.sprite = Sprites[Skill_3];
     }
 }
